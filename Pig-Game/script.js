0cc80afc8b50b921da8b7699c4e5c7c6dac38809
GurmanKD <gurmankaur0603@gmail.com>
2023-06-22 14:18:37 +0530

    Add files via upload

---
+++ Pig-Game/script.js
@@ -0,0 +1,57 @@
+'use strict';
+
+// Selecting the elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--0');
+
+const score0El = document.querySelector('#score--0');
+// const score1=document.getElementById('score--0') This works just like the above statement but this is a bit faster.
+const score1El = document.querySelector('score--1');
+
+const current0EL = document.querySelector('#current--0');
+const current1EL = document.querySelector('#current--1');
+
+const diceEl = document.querySelector('.dice');
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+
+let currentScore = 0;
+
+let activePlayer = 0;
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for a rolled 1
+  if (dice !== 1) {
+    //Add dice to the current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    //Switch to the next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+    //toggle method will add the class if it was not previously present and remove if it was present.
+  }
+});
